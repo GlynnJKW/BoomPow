cec7115eb1a5c1c90ae880ffe18c80a0f8502f82
kirrab <kirrabakken@gmail.com>
2017-01-07 16:20:23 -0800

    More Networking

---
+++ Boompow-001/Assets/JoinGame.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    private NetworkManager networkManager;
+
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+
+    }
+
+    public void RefreshRoomList()
+    {
+        ClearRoomList();
+        networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 1, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(ListMatchResponse matchList) 
+     
+    
+    //public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+
+    {
+        status.text = "";
+
+        if (matchList == null)
+        {
+            status.text = "Couldn't get room list.";
+            return;
+        }
+
+
+        foreach (MatchDesc match in matchList.matches)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            RoomListItem _roomListItem = _roomListItemGO.GetComponent<RoomListItem>();
+            if (_roomListItemGO != null)
+            {
+                _roomListItem.Setup(match);
+            }
+
+
+            //as well setting up a callback function that will join the game.
+            roomList.Add(_roomListItemGO);
+        }
+
+        if (roomList.Count == 0)
+        {
+            status.text = "No rooms at the moment.";
+        }
+    }
+
+    void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+    
+    }
+}
---
+++ Boompow-001/Assets/RoomListItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking.Match;
+
+public class RoomListItem : MonoBehaviour {
+
+    public delegate void JoinRoom
+
+    [SerializeField]
+    private Text roomNameText;
+        
+
+    private MatchDesc match;
+
+    public void Setup (MatchDesc_match)
+    {
+       
+        match = _match;
+
+        roomNameText.text = match.name + " (" + match.currentSize + "/" + match.maxSize + ")";
+    }
+
+    public void JoinRoom ()
+    {
+
+    }
+	
+}
