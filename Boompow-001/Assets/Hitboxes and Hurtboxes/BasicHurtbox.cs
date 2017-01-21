79f2991a20d8b3f545d878f1dce2a42b115e91e7
WEDSGARBAGEDAY <gjwillia@UCSD.EDU>
2017-01-20 18:01:23 -0800

    Basic hit and hurtboxes now working

---
+++ Boompow-001/Assets/Hitboxes and Hurtboxes/BasicHurtbox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicHurtbox : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage(float damage)
+    {
+        Debug.Log("Hurtbox took damage: " + damage);
+        player.SendMessage("TakeDamage", damage);
+    }
+}
---
+++ Boompow-001/Assets/Scripts/HitboxController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] hitboxes;
+
+    [SerializeField]
+    private GameObject hbCenter;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ActivateHitbox(int num)
+    {
+        hitboxes[num].SetActive(true);
+    }
+
+    void DeactivateHitbox(int num)
+    {
+        hitboxes[num].SetActive(false);
+        hitboxes[num].GetComponent<BasicHitbox>().resetHit();
+    }
+}
